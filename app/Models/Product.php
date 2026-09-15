968283808f675d9a61f6816e4fb295cfd29f5230
haroonmubasharofficial <haroonmubasharofficial@gmail.com>
2026-09-15 14:09:16 +0500

    fix: add product unit relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,7 +1,16 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Product extends Model
 {
     protected $fillable = ['product_category_id','unit_id','sku','name','description','item_type','brand','model','warranty','purchase_cost','default_selling_price','tax_rate','is_active','track_serial'];
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
 }
