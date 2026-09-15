96fd8a1acfa390952ec72b6f518025057194c4e9
haroonmubasharofficial <haroonmubasharofficial@gmail.com>
2026-09-15 14:11:05 +0500

    feat: add quotation workflow routes

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => redirect()->route('dashboard'));
 Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
+Route::get('/quotations', [QuotationController::class, 'index'])->name('quotations.index');
 Route::get('/quotations/create', [QuotationController::class, 'create'])->name('quotations.create');
 Route::post('/quotations', [QuotationController::class, 'store'])->name('quotations.store');
+Route::get('/quotations/{quotation}', [QuotationController::class, 'show'])->name('quotations.show');
+Route::get('/quotations/{quotation}/edit', [QuotationController::class, 'edit'])->name('quotations.edit');
+Route::put('/quotations/{quotation}', [QuotationController::class, 'update'])->name('quotations.update');
+Route::get('/quotations/{quotation}/print', [QuotationController::class, 'print'])->name('quotations.print');
