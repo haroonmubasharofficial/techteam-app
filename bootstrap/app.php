b0645ea4bee1ae649d8102b247378491c4da3f74
haroonmubasharofficial <haroonmubasharofficial@gmail.com>
2026-09-15 15:49:32 +0500

    Register role authorization middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureUserActive;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias(['active.user' => EnsureUserActive::class]);
+        $middleware->alias([
+            'active.user' => EnsureUserActive::class,
+            'role' => RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Exception configuration.
