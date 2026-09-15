3ebe4bca4b49fb49f20bd348366371263490bc9d
haroonmubasharofficial <haroonmubasharofficial@gmail.com>
2026-09-15 15:47:44 +0500

    Register active-user middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureUserActive;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Application middleware will be added here as the system grows.
+        $middleware->alias(['active.user' => EnsureUserActive::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Exception configuration.
